c66b35dbc6feea27565369ba9c29b3ae3f9559ca
Na Seonkyung <seonseon1515@gmail.com>
2024-03-26 18:37:46 +0900

    헤더 레이아웃

---
+++ front/src/components/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../css/Header.scss'
+
+export default function Header() {
+  return (
+    <header id='link_header'>
+      <div className='wrap left_link'><Link to='/' className='link_mian'>메인페이지 로고</Link></div>
+      <div className='wrap middle_link'>
+        <div><Link to='/tutor' className='link_tutor'>원어민 튜터 소개 및 검색 페이지</Link></div>
+        <div><Link to='/speaking' className='link_speaking'>학습페이지</Link></div>
+        <div><Link to='/study' className='link_study_group'>스터디 그룹 모집 페이지</Link></div>
+        <div><Link to='/QnA' className='link_QnA'>질문 및 고민 페이지</Link></div>
+      </div>
+      <div className='wrap right_link'><Link to="/mypage">마이페이지 로고</Link></div>
+    </header>
+  )
+}
---
+++ front/src/pages/Page4.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Page4() {
+  return (
+    <div>page4</div>
+  )
+}
