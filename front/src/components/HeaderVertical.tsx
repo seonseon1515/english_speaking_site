ca13503bda0c822f7b41466bec2ac99c762ec3ad
Na Seonkyung <seonseon1515@gmail.com>
2024-04-04 13:20:58 +0900

    글 쓰기 라이브러리 사용하여 글 작성 컴포넌트 생성

--- front/src/components/HeaderVertical.tsx
+++ front/src/components/HeaderVertical.tsx
@@ -9,7 +9,7 @@ export default function HeaderVerical() {
     <header id='vertical'>
       <div className='wrap top_link'>
         <div><Link to='/native' className='link_tutor'>원어민 튜터 소개 및 검색 페이지</Link></div>
-        <div><Link to='/speaking' className='link_speaking'>학습페이지</Link></div>
+        <div><Link to='/introduce' className='link_speaking'>과정 소개</Link></div>
         <div><Link to='/study' className='link_study_group'>스터디 그룹 모집 페이지</Link></div>
         <div><Link to='/QnA' className='link_QnA'>질문 및 고민 페이지</Link></div>
       </div>
---
+++ front/src/pages/CourseRegisterPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CourseRegisterPage() {
+  return (
+    <div>
+      <h1>수강 신청 페이지</h1>
+    </div>
+  )
+}
