4b0edfad8ec7e7d86283cfb71c08896a3bd00fbd
Na Seonkyung <seonseon1515@gmail.com>
2024-04-05 09:40:22 +0900

    css 폰트 사이즈 및 이미지 크기 수정

--- front/src/components/introduce/CourseMenu.tsx
+++ front/src/components/introduce/CourseMenu.tsx
@@ -33,7 +33,7 @@ export default function CourseMenu() {
       <div onClick={()=>clickedCourseMenu(setBusinessMenu)} style={businessMenu} className='courseMenu business'><img className='courseMenuImg' src={`${process.env.PUBLIC_URL}/img/business.png`} alt="business" /><p className='courseText'>비지니스</p></div>
       <div onClick={()=>clickedCourseMenu(setOpicMenu)} style={opicMenu} className='courseMenu opic'><img className='courseMenuImg' src={`${process.env.PUBLIC_URL}/img/opic.png`} alt="opic" /><p className='courseText'>오픽</p></div>
       <div onClick={()=>clickedCourseMenu(setToeicMenu)} style={toeicMenu} className='courseMenu toeic'><img className='courseMenuImg' src={`${process.env.PUBLIC_URL}/img/toeic.png`} alt="toeic" /><p className='courseText'>토익</p></div>
-      <div onClick={()=>clickedCourseMenu(setNewsMenu)} style={newsMenu} className='courseMenu news'><img className='courseMenuImg' src={`${process.env.PUBLIC_URL}/img/news.png`} alt="news" /><p className='courseText'>뉴스</p></div>
+      <div onClick={()=>clickedCourseMenu(setNewsMenu)} style={newsMenu} className='courseMenu news'><img className='courseMenuImg news' src={`${process.env.PUBLIC_URL}/img/news.png`} alt="news" /><p className='courseText'>뉴스</p></div>
       <div onClick={()=>clickedCourseMenu(setInterviewMenu)} style={interviewMenu} className='courseMenu interview'><img className='courseMenuImg' src={`${process.env.PUBLIC_URL}/img/interview.png`} alt="interview" /><p className='courseText'>면접</p></div>
       <div onClick={()=>clickedCourseMenu(setJuniorMenu)} style={juniorMenu} className='courseMenu junior'><img className='courseMenuImg' src={`${process.env.PUBLIC_URL}/img/junior.png`} alt="junior" /> <p className='courseText'>주니어</p></div>
     </div>
